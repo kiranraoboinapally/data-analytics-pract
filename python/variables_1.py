b4dfc7345b8c0f5173509e88110eb350352ad3a4
GPTContribX <38401453+kiranraoboinapally@users.noreply.github.com>
2025-07-10 21:15:41 +0530

    Create variables_1.py

---
+++ python/variables_1.py
@@ -0,0 +1,15 @@
+#Varibales are like containers that stores/holds the data.
+#Store information such as Integer,String,Float,Boolean.
+
+#Key Concepts:
+#1.Variable Assignment: Assigning a value to a variable is done with the = operator.
+#2.Dynamic Typing: Python automatically determines the data type of a variable based on the value assigned.
+#3.Naming Variables: Variables must start with a letter or an underscore and can contain letters, numbers, and underscores (but cannot start with a number).
+
+name="Kiran"
+age=25
+is_adult=True
+_isUnemployed=True
+
+
+
